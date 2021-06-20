b32a8df134af61bd0f8aaa3e3b9f792970ae7291
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-20 19:46:22 +0530

    Added DataBase and its test

---
+++ exekute/core/db.py
@@ -0,0 +1,50 @@
+import json
+import os
+from json.decoder import JSONDecodeError
+from pathlib import Path
+from typing import Dict
+from typing import List
+from typing import Union
+
+from .const import DB_PATH
+
+DBType = Dict[str, Dict[str, Union[List[str], str]]]
+
+
+class DataBase:
+
+    def __init__(self) -> None:
+        self.db = os.path.join(DB_PATH, "data.json")
+
+        if not Path(self.db).is_file():
+            self.__create_db()
+
+        self.data = self.__load_data()
+
+    def get_all(self) -> DBType:
+        return self.data
+
+    def add(self, _id: str, data: Dict[str, Union[List[str], str]]) -> None:
+        self.data[_id] = data
+        self.__dump_data()
+
+    def get_by_id(self, _id: str) -> Dict[str, Union[List[str], str, int]]:
+        if _id in self.data:
+            return self.data[_id]
+
+    def __create_db(self) -> None:
+        with open(self.db, "w") as f:
+            f.write("{}")
+
+    def __load_data(self) -> DBType:
+        try:
+            with open(self.db, "r") as f:
+                data = json.load(f)
+                return data
+        except JSONDecodeError:
+            self.__create_db()
+            return self.__load_data()
+
+    def __dump_data(self) -> None:
+        with open(self.db, "w") as f:
+            json.dump(self.data, f)
---
+++ test/test_db.py
@@ -0,0 +1,80 @@
+import json
+import os
+from pathlib import Path
+
+import pytest
+
+from exekute.core.db import DataBase
+
+test_data = {
+    "3425345": {
+        "name": "project1",
+                "apps": ["test.exe", "name.exe"],
+                "script": ""
+    },
+    "123456": {
+        "name": "project2",
+                "apps": ["p.exe", "n.exe"],
+                "script": "test.bat"
+    },
+}
+
+
+@pytest.fixture
+def json_file():
+    with open("test.json", "w") as f:
+        json.dump(test_data, f)
+    yield "test.json"
+    os.remove("test.json")
+
+
+def test_database_get_all(json_file: str, mocker) -> None:
+    mocker.patch("os.path.join", return_value=json_file)
+    a = DataBase()
+    assert a.get_all() == test_data
+
+
+def test_database_get_by_id(json_file: str, mocker) -> None:
+    mocker.patch("os.path.join", return_value=json_file)
+    a = DataBase()
+    assert a.get_by_id("3425345") == test_data["3425345"]
+
+
+def test_database_add_no_id_exists(json_file, mocker) -> None:
+    mocker.patch("os.path.join", return_value=json_file)
+    a = DataBase()
+    demo_data = {"name": "project2", "apps": ["te.exe"], "script": "hello.bat"}
+    a.add("098765", demo_data)
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+        assert data["098765"] == demo_data
+        assert len(data) == 3
+
+
+def test_database_add_id_exists(json_file, mocker) -> None:
+    mocker.patch("os.path.join", return_value=json_file)
+    a = DataBase()
+    demo_data = {"name": "project2", "apps": ["te.exe"], "script": "hello.bat"}
+    a.add("123456", demo_data)
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+        assert data["123456"] == demo_data
+        assert len(data) == 2
+
+
+def test_database_file_not_exist(mocker) -> None:
+    mocker.patch("os.path.join", return_value="test.json")
+    _ = DataBase()
+    assert Path("test.json").is_file() is True
+
+
+def test_database_json_decode_error(json_file: str, mocker) -> None:
+    mocker.patch("os.path.join", return_value=json_file)
+    with open(json_file, "w") as f:
+        f.write("")
+    _ = DataBase()
+
+    with open(json_file, "r") as f:
+        assert f.read() == "{}"
